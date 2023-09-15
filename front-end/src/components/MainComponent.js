116adee91735329f9f6940a59cfb4fc8077e79c0
Casper Tyson <caspertyson99@gmail.com>
2023-09-16 10:58:41 +1200

    added in map for sent messages

--- front-end/src/components/MainComponent.js
+++ front-end/src/components/MainComponent.js
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import '../style/MainComponent.css';
 import New from '../Images/edit.png'
+import Map from '../components/Map'
 
 function MainComponent({ onSendToggle, onShowMap,user }) {
 
   const [inboxSent, setInboxSent] = useState("Inbox")
-  const inbox = [["Lenny", "My Pigeon is superior to yours","25 Aug"],
-  ["John", "My Pigeon is superior to yours","25 Aug"],
-  ["Carmen", "My Pigeon is superior to yours","25 Aug"],]
+  // const inbox = [["Lenny", "My Pigeon is superior to yours","25 Aug"],
+  // ["John", "My Pigeon is superior to yours","25 Aug"],
+  // ["Carmen", "My Pigeon is superior to yours","25 Aug"],]
   
-  const sent = [["Lenny", "Wanna Have a pigeon off","Est. Delivery: 25 Aug, 2024"],
-  ["John", "Wanna Have a pigeon off","Delivered: 25 Aug, 2024"],]
+  // const sent = [["Lenny", "Wanna Have a pigeon off","Est. Delivery: 25 Aug, 2024"],
+  // ["John", "Wanna Have a pigeon off","Delivered: 25 Aug, 2024"],]
 
-  const messages = inboxSent === 'Inbox' ? inbox : sent;
+  // const messages = inboxSent === 'Inbox' ? inbox : sent;
 
   const [inboxMessages, setInboxMessages] = useState({ sent_messages: [], received_messages: [] });
+  const [selectedMessageId, setSelectedMessageId] = useState(null);
 
   useEffect(() => {
     fetch('https://otk78wgmid.execute-api.ap-southeast-2.amazonaws.com/develop/api/message?user=John+Doe')
@@ -67,13 +69,29 @@ function MainComponent({ onSendToggle, onShowMap,user }) {
           <div id='mainComponentDisplay'>
             <div id='inboxSentTitle'>{inboxSent}</div>
             <div>
-              {inboxMessages.received_messages.map((msg, index) => (
-                <div key={index} className='receivedMail'>
-                  <span className='senderName'>{msg.senderName}</span>
-                  <span className='senderMessage'>{msg.message}</span>
-                  <span className='senderDate'>{formatDate(msg.sentDate)}</span>
-                </div>
-              ))}
+              {inboxSent === 'Sent' ? (
+                // Display sent messages
+                inboxMessages.sent_messages.map((msg, index) => (
+                  <>
+                  <div key={msg.message_id} className='receivedMail' onClick={() => setSelectedMessageId(msg.message_id)}>
+                    <span className='senderName'>{msg.receiverName}</span>
+                    <span className='senderMessage'>{msg.message}</span>
+                    <span className='senderDate'>{formatDate(msg.sentDate)}</span>
+                  </div>
+                  {selectedMessageId === msg.message_id && <Map message_id={selectedMessageId} />}
+
+                  </>
+                ))
+              ) : (
+                // Display received messages
+                inboxMessages.received_messages.map((msg, index) => (
+                  <div key={msg.message_id} className='receivedMail'>
+                    <span className='senderName'>{msg.senderName}</span>
+                    <span className='senderMessage'>{msg.message}</span>
+                    <span className='senderDate'>{formatDate(msg.sentDate)}</span>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
--- front-end/src/components/Map.js
+++ front-end/src/components/Map.js
@@ -1,57 +1,99 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, ZoomControl } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css'; // Don't forget to import leaflet's CSS
-import '../style/Map.css'
-import L from 'leaflet';  // Import Leaflet library
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Polyline, Marker, ZoomControl } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../style/Map.css';
+import L from 'leaflet';
 import Pigeon from '../Images/landingPagePigeon.png'
 
-
-// Optional: You could place these styles in a CSS file
 const mapStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: '500px',
+  height: '300px',
   position: 'absolute'
 };
-const generateRandomPoints = (numPoints) => {
-    const points = [];
-    for (let i = 0; i < numPoints; i++) {
-      const latitude = Math.random() * 180 - 90; // Latitude between -90 and 90
-      const longitude = Math.random() * 360 - 180; // Longitude between -180 and 180
-      points.push([latitude, longitude]);
+
+// const generateRandomPoints = (numPoints) => {
+//   var latlngs = [];
+//   for (var i = 0; i < 4; i++) {
+//     var lat = -43 + (Math.random() - 0.5) * 20;
+//     var lng = 172 + (Math.random() - 0.5) * 20;
+//     latlngs.push([lat, lng]);
+//   }
+//   return latlngs;
+// };
+
+// const randomPoints = generateRandomPoints(100);
+
+const MapComponent = ({ onCloseMap }) => {
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [points, setPoints] = useState([[0, 0]]);
+
+  const customIcon = new L.Icon({
+    iconUrl: Pigeon,
+    iconRetinaUrl: Pigeon,
+    iconSize: [25, 41],  // size of the icon
+    iconAnchor: [12.5, 41],  // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -41]  // point from which the popup should open relative to the iconAnchor
+  });
+
+  useEffect(() => {
+    fetch('https://otk78wgmid.execute-api.ap-southeast-2.amazonaws.com/develop/api/message/81ff659df2e15226bffe29a517e20cc951d98b47cb342b1f183da0493d33901c/location')
+    .then(response => response.json())
+    .then(data => {
+      const transformedPoints = data.map(point => [point.latitude, point.longitude]);
+      setPoints(transformedPoints);
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  }, []);
+  useEffect(() => {
+    if (points) {
+      drawPoints();
     }
-    return points;
-  };
+  }, [points]);
   
-  const randomPoints = generateRandomPoints(100);
+  const drawPoints = () => {
+    const startTime = new Date('2023-09-16T10:00:12').getTime();
+    const arrivalTime = new Date('2023-09-16T10:30:12').getTime();
+    const currentTime = new Date().getTime();
   
-const MapComponent = ({onCloseMap}) => {
-    const customIcon = new L.Icon({
-        iconUrl: Pigeon,
-        iconRetinaUrl: Pigeon,
-        iconSize: [25, 41],  // size of the icon
-        iconAnchor: [12.5, 41],  // point of the icon which will correspond to marker's location
-        popupAnchor: [0, -41]  // point from which the popup should open relative to the iconAnchor
-      });
-    
+    const journeyDuration = arrivalTime - startTime;
+    const timeElapsed = currentTime - startTime;
+  
+    const percentageCompleted = Math.min(timeElapsed / journeyDuration, 1);
+  
+    const totalPoints = points.length;
+    const pointIndex = Math.floor(percentageCompleted * (totalPoints - 1));
+    const nextPointIndex = Math.min(pointIndex + 1, totalPoints - 1);  // Ensure it's within bounds
+  
+    const startPoint = points[pointIndex];
+    const endPoint = points[nextPointIndex];
+  
+    if (startPoint && endPoint) {  // Check if startPoint and endPoint are defined
+      const fraction = (percentageCompleted * (totalPoints - 1)) - pointIndex;
+  
+      const interpolatedLat = startPoint[0] + fraction * (endPoint[0] - startPoint[0]);
+      const interpolatedLng = startPoint[1] + fraction * (endPoint[1] - startPoint[1]);
+  
+      setCurrentPosition([interpolatedLat, interpolatedLng]);
+    }
+  }
+
   return (
     <div id='mapContainer'>
-        <MapContainer
-        center={[0, 0]}
-        zoom={2}
-        style={mapStyle}
-        >
+      <MapContainer center={points[0]} zoom={1} style={mapStyle}>
         <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {randomPoints.map((point, index) => (
-          <Marker key={index} position={point} icon={customIcon} />
-        ))}
-        </MapContainer>
-        <div >
-            <button onClick={onCloseMap} id="mapBackButton" >Back</button>
-        </div>
-
+        <Polyline positions={points} color='blue' />
+        {currentPosition && <Marker position={currentPosition} icon={customIcon} />}
+      </MapContainer>
+      <div>
+        {/* <button onClick={onCloseMap} id="mapBackButton">Back</button> */}
+      </div>
     </div>
   );
 };
